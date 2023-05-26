ef3ff18bf648dbe6ce3875b93a5a5b3e08d18e5d
v1tal4ik <dariia.zhukovska@gmail.com>
2023-05-26 23:03:58 +0300

    added rtk to proj

---
+++ store.js
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import { configureStore } from '@reduxjs/toolkit';
+
+import * as api from './config';
+import { themeReducer } from './features/theme/theme-slice';
+import { controlsReducer } from './features/controls/controls-slice';
+import { countriesReducer } from './features/countries/countries-slice';
+
+export const store = configureStore({
+  reducer: {
+    theme: themeReducer,
+    controls: controlsReducer,
+    countries: countriesReducer,
+
+  },
+  devTools: true,
+  middleware: (getDefaultMiddlware) => getDefaultMiddlware({
+    thunk: {
+      extraArgument: {
+        client: axios,
+        api,
+      },
+    },
+    serializableCheck: false,
+  })
+});
